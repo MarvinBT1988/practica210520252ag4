ec24c52bc5472ab53748aa6bc7ded38fab4d3848
Franjse <antoniobtrigueros@gmail.com>
2025-05-21 16:04:40 -0600

    ghjhjhgj

--- registroacademico/Maestro.java
+++ registroacademico/Maestro.java
@@ -1,5 +1,37 @@
 package registroacademico;
 
-public class Maestro {
+public class Maestro  extends DatoGeneral{
+    private String dui;
+    private String grados;
+    public Maestro(){
 
+    }
+    public Maestro(String nombre, String direccion, String telefono, String email, String dui, String grados) {
+        super(nombre, direccion, telefono, email);
+        this.dui = dui;
+        this.grados = grados;
+    }
+    public String getDui() {
+        return dui;
+    }
+    public void setDui(String dui) {
+        this.dui = dui;
+    }
+    public String getGrados() {
+        return grados;
+    }
+    public void setGrados(String grados) {
+        this.grados = grados;
+    }
+    public void pasarLista() {
+        System.out.println("Pasando lista del maestro " + getNombre() + ":");
+        // Aquí se puede implementar la lógica para pasar lista
+    }
+    @Override
+    public void modificar() {
+        System.out.println("Modificación de maestro:");
+        System.out.println("Número de DUI: " + dui);
+        System.out.println("Grados: " + grados);
+    }
+    
 }
