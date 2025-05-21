3a29c30a0be69bb899db1362d00aa9dd985145ac
Franjse <antoniobtrigueros@gmail.com>
2025-05-21 15:41:32 -0600

    sfsfsf

--- registroacademico/Alumno.java
+++ registroacademico/Alumno.java
@@ -1,5 +1,26 @@
 package registroacademico;
 
-public class Alumno {
-
+public class Alumno extends DatoGeneral {
+    private String nCarnet;
+    private String encargado;
+    public Alumno() {
+    }
+    public Alumno(String nombre, String direccion, String telefono, String email, String nCarnet, String encargado) {
+        super(nombre, direccion, telefono, email);
+        this.nCarnet = nCarnet;
+        this.encargado = encargado;
+    }
+    public String getnCarnet() {
+        return nCarnet;
+    }
+    public void setnCarnet(String nCarnet) {
+        this.nCarnet = nCarnet;
+    }
+    public String getEncargado() {
+        return encargado;
+    }
+    public void setEncargado(String encargado) {
+        this.encargado = encargado;
+    }
+    
 }
