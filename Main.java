32ad7bda767eebef1301291fe26cdecca27d8a36
Franjse <antoniobtrigueros@gmail.com>
2025-05-21 16:08:25 -0600

    fffg fgfgh

--- Main.java
+++ Main.java
@@ -1,3 +1,18 @@
+import registroacademico.Alumno;
+import registroacademico.Maestro;
 public class Main {
+    public static void main(String[] args) {
+        Alumno alumno = new Alumno();
+        alumno.setNombre("Juan Perez");
+        alumno.setDireccion("Calle 123");
+        alumno.registrar();
+        alumno.modificar();
 
+        Maestro maestro = new Maestro();
+        maestro.setNombre("Maria Lopez");   
+        maestro.setDireccion("Avenida 456");
+        maestro.registrar();
+        maestro.modificar();
+    
+    }
 }
